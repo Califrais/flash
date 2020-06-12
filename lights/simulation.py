f573411bc8aff526e0bb2b73f2279ef0c945617f
SimonBussy <simon.bussy@gmail.com>
2020-06-12 17:18:07 +0200

    data simulation on process with tests

---
+++ lights/simulation.py
@@ -0,0 +1,333 @@
+# -*- coding: utf-8 -*-
+# Author: Simon Bussy <simon.bussy@gmail.com>
+
+from datetime import datetime
+from time import time
+from scipy.linalg.special_matrices import toeplitz
+from tick.hawkes import SimuHawkesExpKernels
+import numpy as np
+
+
+def features_normal_cov_toeplitz(n_samples: int = 200, n_features: int = 10,
+                                 rho: float = 0.5):
+    """Features obtained as samples of a centered Gaussian vector
+    with a toeplitz covariance matrix
+
+    Parameters
+    ----------
+    n_samples : `int`, default=200
+        Number of samples
+
+    n_features : `int`, default=10
+        Number of features
+
+    rho : `float`, default=0.5
+        Correlation coefficient of the toeplitz correlation matrix
+
+    Returns
+    -------
+    output : `np.ndarray`, shape=(n_samples, n_features)
+        The simulated features matrix
+    """
+    cov = toeplitz(rho ** np.arange(0, n_features))
+    return np.random.multivariate_normal(
+        np.zeros(n_features), cov, size=n_samples)
+
+
+def simulation_method(simulate_method):
+    """A decorator for simulation methods.
+    It simply calls _start_simulation and _end_simulation methods
+    """
+
+    def decorated_simulate_method(self):
+        self._start_simulation()
+        result = simulate_method(self)
+        self._end_simulation()
+        self.data = result
+        return result
+
+    return decorated_simulate_method
+
+
+class Simulation:
+    """This is an abstract simulation class that inherits form BaseClass
+    It does nothing besides printing stuff and verbosing
+    """
+
+    def __init__(self, seed=None, verbose=True):
+        # Set default parameters
+        self.seed = seed
+        self.verbose = verbose
+        if self.seed is not None:
+            self._set_seed()
+        # No data simulated yet
+        self.features = None
+        self.labels = None
+
+    def _set_seed(self):
+        np.random.seed(self.seed)
+        return self
+
+    @staticmethod
+    def _get_now():
+        return str(datetime.now()).replace(" ", "_").replace(":", "-")
+
+    def _start_simulation(self):
+        self.time_start = Simulation._get_now()
+        self._numeric_time_start = time()
+        if self.verbose:
+            msg = "Launching simulation using {class_}..." \
+                .format(class_=self.__class__.__name__)
+            print("-" * len(msg))
+            print(msg)
+
+    def _end_simulation(self):
+        self.time_end = self._get_now()
+        t = time()
+        self.time_elapsed = t - self._numeric_time_start
+        if self.verbose:
+            msg = "Done simulating using {class_} in {time:.2e} seconds." \
+                .format(class_=self.__class__.__name__,
+                        time=self.time_elapsed)
+            print(msg)
+
+
+class SimuJointLongitudinalSurvival(Simulation):
+    """Class for the simulation of Joint High-dimensional Longitudinal and
+    Survival Data
+
+    Parameters
+    ----------
+    verbose : `bool`, default=True
+        Verbose mode to detail or not ongoing tasks
+
+    seed : `int`, default=None
+        The seed of the random number generator, for reproducible simulation. If
+        `None` it is not seeded
+
+    n_samples : `int`, default=200
+        Number of samples
+
+    n_time_indep_features : `int`, default=20
+        Number of time-independent features
+
+    time_indep_sparsity : `float`, default=0.5
+        Percentage of sparsity induced in the time-independent coefficient
+        vector. Must be in [0, 1].
+
+    time_indep_coeff : `float`, default=1
+        Value of the active coefficients in the time-independent coefficient
+        vector.
+
+    cov_corr_time_indep : `float`, default=0.5
+        Correlation to use in the Toeplitz covariance matrix for the
+        time-independent features
+
+    low_risk_rate : `float`, default=0.75
+        Proportion of desired low risk samples rate
+
+    gap : `float`, default=0.1
+        Gap value to create high/low risk groups in the time-independent
+        features
+
+    n_long_features : `int`, default=5
+        Number of longitudinal features
+
+    cov_corr_long : `float`, default=0.5
+        Correlation to use in the Toeplitz covariance matrix for the random
+        effects simulation, as well as for the adjacency matrix to simulate the
+        measurement times using multivariate Hawkes processes
+
+    corr_fixed_effect : `float`, default=0.5
+        Correlation value to use in the diagonal covariance matrix for the
+        fixed effect simulation
+
+    var_error : `float`, default=0.5
+        Variance for the error term of the longitudinal process
+
+    hawkes_memory:
+        Must be positive
+
+
+
+    scale : `float`, default=1.0
+        Scaling parameter to use in the distribution of times
+
+    shape : `float`, default=1.0
+        Shape parameter to use in the distribution of times
+
+    censoring_factor : `float`, default=2.0
+        Level of censoring. Increasing censoring_factor leads
+        to less censored times and conversely.
+
+    Attributes
+    ----------
+    times : `np.ndarray`, shape=(n_samples,)
+        The simulated times of the event of interest
+
+    latent_class : `np.ndarray`, shape=(n_samples,)
+        The simulated latent classes
+
+    censoring : `np.ndarray`, shape=(n_samples,)
+        The simulated censoring indicator, where ``censoring[i] == 1``
+        indicates that the time of the i-th individual is a failure
+        time, and where ``censoring[i] == 0`` means that the time of
+        the i-th individual is a censoring time
+
+    Notes
+    -----
+    There is no intercept in this model
+    """
+
+    def __init__(self, verbose: bool = True, seed: int = None,
+                 n_samples: int = 200, n_time_indep_features: int = 20,
+                 time_indep_sparsity: float = 0.5, time_indep_coeff: float = 1.,
+                 cov_corr_time_indep: float = 0.5, low_risk_rate: float = .75,
+                 gap: float = .1, n_long_features: int = 5,
+                 cov_corr_long: float = 0.5, corr_fixed_effect: float = 0.5,
+                 var_error: float = 0.5,
+
+                 shape: float = 1.,
+                 scale: float = 1.,
+                 censoring_factor: float = 2.):
+        Simulation.__init__(self, seed=seed, verbose=verbose)
+
+        self.n_samples = n_samples
+        self.n_time_indep_features = n_time_indep_features
+        self.time_indep_sparsity = time_indep_sparsity
+        self.time_indep_coeff = time_indep_coeff
+        self.time_indep_sparsity = time_indep_sparsity
+        self.cov_corr_time_indep = cov_corr_time_indep
+        self.low_risk_rate = low_risk_rate
+        self.gap = gap
+        self.shape = shape
+        self.scale = scale
+        self.censoring_factor = censoring_factor
+
+        # Attributes that will be instantiated afterwards
+        self.T = None
+        self.G = None
+        self.delta = None
+
+    @property
+    def shape(self):
+        return self._shape
+
+    @shape.setter
+    def shape(self, val):
+        if val <= 0:
+            raise ValueError("``shape`` must be strictly positive")
+        self._shape = val
+
+    @property
+    def scale(self):
+        return self._scale
+
+    @scale.setter
+    def scale(self, val):
+        if val <= 0:
+            raise ValueError("``scale`` must be strictly positive")
+        self._scale = val
+
+    @property
+    def time_indep_sparsity(self):
+        return self._time_indep_sparsity
+
+    @time_indep_sparsity.setter
+    def time_indep_sparsity(self, val):
+        if not 0 <= val <= 1:
+            raise ValueError("``time_indep_sparsity`` must be in (0, 1)")
+        self._time_indep_sparsity = val
+
+    @staticmethod
+    def logistic_grad(z):
+        """Overflow proof computation of 1 / (1 + exp(-z)))
+        """
+        idx_pos = np.where(z >= 0.)
+        idx_neg = np.where(z < 0.)
+        res = np.empty(z.shape)
+        res[idx_pos] = 1. / (1. + np.exp(-z[idx_pos]))
+        res[idx_neg] = 1 - 1. / (1. + np.exp(z[idx_neg]))
+        return res
+
+    @simulation_method
+    def simulate(self):
+        """Launch simulation of the data
+
+        Returns
+        -------
+        X : `numpy.ndarray`, shape=(n_samples, n_time_indep_features)
+            The simulated time-independent features matrix
+
+        Y :
+            The simulated longitudinal data
+
+        T : `np.ndarray`, shape=(n_samples,)
+            The simulated times of the event of interest
+
+        delta : `np.ndarray`, shape=(n_samples,)
+            The simulated censoring indicator
+        """
+        n_samples = self.n_samples
+        n_time_indep_features = self.n_time_indep_features
+        time_indep_sparsity = self.time_indep_sparsity
+        time_indep_coeff = self.time_indep_coeff
+        low_risk_rate = self.low_risk_rate
+        gap = self.gap
+        cov_corr_time_indep = self.cov_corr_time_indep
+
+        nb_active_features = int(n_time_indep_features * time_indep_sparsity)
+
+        xi = np.zeros(n_time_indep_features)
+        xi[0:nb_active_features] = time_indep_coeff
+
+        features = features_normal_cov_toeplitz(n_samples,
+                                                n_time_indep_features,
+                                                cov_corr_time_indep)
+
+        # Add class relative information on the design matrix    
+        H = np.random.choice(range(n_samples),
+                             size=int((1 - low_risk_rate) * n_samples),
+                             replace=False)
+        H_ = np.delete(range(n_samples), H)
+        features[H, :nb_active_features] += gap
+        features[H_, :nb_active_features] -= gap
+        self.features = features
+
+        # Simulation of latent variables
+        pi = self.logistic_grad(-features.dot(xi))
+        u = np.random.rand(n_samples)
+        self.G = u <= 1 - pi
+
+        ##
+        n_nodes = 3  # dimension of the Hawkes process
+        adjacency = 0.2 * np.ones((n_nodes, n_nodes))
+        adjacency[0, 1] = 0
+        decays = 3 * np.ones((n_nodes, n_nodes))
+        baseline = 0.5 * np.ones(n_nodes)
+        hawkes = SimuHawkesExpKernels(adjacency=adjacency, decays=decays,
+                                      baseline=baseline, verbose=False,
+                                      seed=2398)
+
+        run_time = 100
+        hawkes.end_time = run_time
+        dt = 0.01
+        hawkes.track_intensity(dt)
+        hawkes.simulate()
+
+
+        ##
+
+        # Simulation of true times
+        T = np.empty(n_samples)
+
+        m = T.mean()
+        # Simulation of the censoring
+        c = self.censoring_factor
+        C = np.random.exponential(scale=c * m, size=n_samples)
+        # Observed time
+        self.times = np.minimum(T, C).astype(int)
+        # Censoring indicator: 1 if it is a time of failure, 0 if censoring
+        censoring = (T <= C).astype(np.ushort)
+        self.censoring = censoring
+        return self.features, self.times, self.censoring
---
+++ lights/tests.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# Author: Simon Bussy <simon.bussy@gmail.com>
+
+import unittest
+from lights.simulation import SimuJointLongitudinalSurvival
+import numpy as np
+
+
+class Test(unittest.TestCase):
+
+    def test_SimuJointLongitudinalSurvival(self):
+        """Test simulation of joint longitudinal and survival data
+        """
+        # Simulate data with specific seed
+        simu = SimuJointLongitudinalSurvival(n_samples=3, n_features=10,
+                                             seed=123, verbose=False)
+        features_, times_, censoring_ = simu.simulate()
+
+        times = np.array([
+            1.5022119, 5.93102441, 6.82837051, 0.50940341, 0.14859682,
+            30.22922996, 3.54945974, 0.8671229, 1.4228358, 0.11483298
+        ])
+
+        censoring = np.array([1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+                             dtype=np.ushort)
+
+        features = np.array([[1.4912667, 0.80881799, 0.26977298], [
+            1.23227551, 0.50697013, 1.9409132
+        ], [1.8891494, 1.49834791,
+            2.41445794], [0.19431319, 0.80245126, 1.02577552], [
+                                 -1.61687582, -1.08411865, -0.83438387
+                             ], [2.30419894, -0.68987056,
+                                 -0.39750262],
+                             [-0.28826405, -1.23635074, -0.76124386], [
+                                 -1.32869473, -1.8752391, -0.182537
+                             ], [0.79464218, 0.65055633, 1.57572506],
+                             [0.71524202, 1.66759831, 0.88679047]])
+
+        np.testing.assert_almost_equal(features, features_)
+        np.testing.assert_almost_equal(times, times_)
+        np.testing.assert_almost_equal(censoring, censoring_)
+
+    @staticmethod
+    def get_train_data(seed: int = 1):
+        """Get train data for specific tests
+        """
+        np.random.seed(seed)
+        simu = SimuJointLongitudinalSurvival()
+        features, times, censoring = simu.simulate()
+        return features, times, censoring
+
+
+if __name__ == "main":
+    unittest.main()
