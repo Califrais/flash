1329b659ad17438b14f2367a8867bab615527cb6
SimonBussy <simon.bussy@gmail.com>
2020-06-16 15:08:39 +0200

    simulation of all param vectors ok

--- lights/simulation.py
+++ lights/simulation.py
@@ -63,8 +63,10 @@ class Simulation:
         if self.seed is not None:
             self._set_seed()
         # No data simulated yet
-        self.features = None
-        self.labels = None
+        self.time_indep_features = None
+        self.long_features = None
+        self.times = None
+        self.censoring = None
 
     def _set_seed(self):
         np.random.seed(self.seed)
@@ -113,13 +115,13 @@ class SimuJointLongitudinalSurvival(Simulation):
     n_time_indep_features : `int`, default=20
         Number of time-independent features
 
-    time_indep_sparsity : `float`, default=0.5
-        Percentage of sparsity induced in the time-independent coefficient
-        vector. Must be in [0, 1].
+    sparsity : `float`, default=0.7
+        Proportion of both time-independent and association features active
+        coefficients vector. Must be in [0, 1].
 
-    time_indep_coeff : `float`, default=1
-        Value of the active coefficients in the time-independent coefficient
-        vector.
+    coeff_val : `float`, default=1
+        Value of the active coefficients in both the time-independent and
+        association coefficient vectors
 
     cov_corr_time_indep : `float`, default=0.5
         Correlation to use in the Toeplitz covariance matrix for the
@@ -158,30 +160,39 @@ class SimuJointLongitudinalSurvival(Simulation):
         Bounds of the uniform distribution used to generate sparse adjacency
         matrix for measurement times intensities
 
+    scale : `float`, default=2.
+        Scaling parameter of the Weibull distribution of the baseline
 
-    scale : `float`, default=1.0
-        Scaling parameter to use in the distribution of times
-
-    shape : `float`, default=1.0
-        Shape parameter to use in the distribution of times
+    shape : `float`, default=.1
+        Shape parameter of the Weibull distribution of the baseline
 
     censoring_factor : `float`, default=2.0
-        Level of censoring. Increasing censoring_factor leads
-        to less censored times and conversely.
+        Level of censoring. Increasing censoring_factor leads to less censored
+        times and conversely.
 
     Attributes
     ----------
-    times : `np.ndarray`, shape=(n_samples,)
-        The simulated times of the event of interest
+    time_indep_features : `np.array`, shape=(n_samples, n_time_indep_features)
+        Simulated time-independent features
 
-    latent_class : `np.ndarray`, shape=(n_samples,)
-        The simulated latent classes
+    long_features :
+        Simulated longitudinal features
+
+    times : `np.ndarray`, shape=(n_samples,)
+        Simulated times of the event of interest
 
     censoring : `np.ndarray`, shape=(n_samples,)
-        The simulated censoring indicator, where ``censoring[i] == 1``
+        Simulated censoring indicator, where ``censoring[i] == 1``
         indicates that the time of the i-th individual is a failure
         time, and where ``censoring[i] == 0`` means that the time of
         the i-th individual is a censoring time
+
+    latent_class : `np.ndarray`, shape=(n_samples,)
+        Simulated latent classes
+
+    hawkes : `tick.hawkes.simulation.simu_hawkes_exp_kernels`
+        Multivariate Hawkes process with exponential kernels used to
+        simulate measurement times
 
     Notes
     -----
@@ -190,22 +201,21 @@ class SimuJointLongitudinalSurvival(Simulation):
 
     def __init__(self, verbose: bool = True, seed: int = None,
                  n_samples: int = 200, n_time_indep_features: int = 20,
-                 time_indep_sparsity: float = 0.5, time_indep_coeff: float = 1.,
+                 sparsity: float = 0.7, coeff_val: float = 1.,
                  cov_corr_time_indep: float = 0.5, low_risk_rate: float = .75,
                  gap: float = .1, n_long_features: int = 5,
                  cov_corr_long: float = 0.5, corr_fixed_effect: float = 0.5,
                  var_error: float = 0.5, decay: float = 3,
                  baseline_hawkes_uniform_bounds: list = (.1, .5),
                  adjacency_hawkes_uniform_bounds: list = (.05, .1),
-                 shape: float = 1.,
-                 scale: float = 1.,
+                 shape: float = 1., scale: float = 1.,
                  censoring_factor: float = 2.):
         Simulation.__init__(self, seed=seed, verbose=verbose)
 
         self.n_samples = n_samples
         self.n_time_indep_features = n_time_indep_features
-        self.time_indep_sparsity = time_indep_sparsity
-        self.time_indep_coeff = time_indep_coeff
+        self.sparsity = sparsity
+        self.coeff_val = coeff_val
         self.cov_corr_time_indep = cov_corr_time_indep
         self.low_risk_rate = low_risk_rate
         self.gap = gap
@@ -216,26 +226,23 @@ class SimuJointLongitudinalSurvival(Simulation):
         self.decay = decay
         self.baseline_hawkes_uniform_bounds = baseline_hawkes_uniform_bounds
         self.adjacency_hawkes_uniform_bounds = adjacency_hawkes_uniform_bounds
-
         self.shape = shape
         self.scale = scale
         self.censoring_factor = censoring_factor
 
         # Attributes that will be instantiated afterwards
+        self.latent_class = None
         self.hawkes = None
-        self.T = None
-        self.G = None
-        self.delta = None
 
     @property
-    def time_indep_sparsity(self):
-        return self._time_indep_sparsity
+    def sparsity(self):
+        return self._sparsity
 
-    @time_indep_sparsity.setter
-    def time_indep_sparsity(self, val):
+    @sparsity.setter
+    def sparsity(self, val):
         if not 0 <= val <= 1:
-            raise ValueError("``time_indep_sparsity`` must be in (0, 1)")
-        self._time_indep_sparsity = val
+            raise ValueError("``sparsity`` must be in (0, 1)")
+        self._sparsity = val
 
     @property
     def low_risk_rate(self):
@@ -246,7 +253,6 @@ class SimuJointLongitudinalSurvival(Simulation):
         if not 0 <= val <= 1:
             raise ValueError("``low_risk_rate`` must be in (0, 1)")
         self._low_risk_rate = val
-
 
     @property
     def shape(self):
@@ -301,8 +307,8 @@ class SimuJointLongitudinalSurvival(Simulation):
         seed = self.seed
         n_samples = self.n_samples
         n_time_indep_features = self.n_time_indep_features
-        time_indep_sparsity = self.time_indep_sparsity
-        time_indep_coeff = self.time_indep_coeff
+        sparsity = self.sparsity
+        coeff_val = self.coeff_val
         cov_corr_time_indep = self.cov_corr_time_indep
         low_risk_rate = self.low_risk_rate
         gap = self.gap
@@ -314,29 +320,68 @@ class SimuJointLongitudinalSurvival(Simulation):
         baseline_hawkes_uniform_bounds = self.baseline_hawkes_uniform_bounds
         adjacency_hawkes_uniform_bounds = self.adjacency_hawkes_uniform_bounds
 
-        nb_time_indep_active_features = int(
-            n_time_indep_features * time_indep_sparsity)
+        # Simulation of time-independent coefficient vector
+        nb_active_time_indep_features = int(n_time_indep_features * sparsity)
         xi = np.zeros(n_time_indep_features)
-        xi[0:nb_time_indep_active_features] = time_indep_coeff
+        xi[0:nb_active_time_indep_features] = coeff_val
 
-        time_indep_features = features_normal_cov_toeplitz(n_samples,
-                                                           n_time_indep_features,
-                                                           cov_corr_time_indep)
-
+        # Simulation of time-independent features
+        X = features_normal_cov_toeplitz(n_samples, n_time_indep_features,
+                                         cov_corr_time_indep)
         # Add class relative information on the design matrix    
         H = np.random.choice(range(n_samples),
                              size=int((1 - low_risk_rate) * n_samples),
                              replace=False)
         H_ = np.delete(range(n_samples), H)
-        time_indep_features[H, :nb_time_indep_active_features] += gap
-        time_indep_features[H_, :nb_time_indep_active_features] -= gap
-        self.features = time_indep_features
+        X[H, :nb_active_time_indep_features] += gap
+        X[H_, :nb_active_time_indep_features] -= gap
+        self.time_indep_features = X
 
         # Simulation of latent variables
-        pi = self.logistic_grad(-time_indep_features.dot(xi))
+        pi = self.logistic_grad(-X.dot(xi))
         u = np.random.rand(n_samples)
-        self.G = u <= 1 - pi
+        G = u <= 1 - pi
+        self.latent_class = G
 
+        # Simulation of the random effects components
+        r = 2 * n_long_features  # linear time-varying features, so all r_l=2
+        b = features_normal_cov_toeplitz(n_samples, r, cov_corr_long)
+
+        # Simulation of the fixed effect parameters
+        q = 2 * n_long_features  # linear time-varying features, so all q_l=2
+        beta_0 = - np.random.multivariate_normal(np.ones(q), np.diag(
+            corr_fixed_effect * np.ones(q)))
+        beta_1 = np.random.multivariate_normal(np.ones(q), np.diag(
+            corr_fixed_effect * np.ones(q)))
+
+        # Simulation of the association parameters
+        nb_asso_features = n_long_features * 4  # 4: nb of asso param
+        nb_active_asso_features = int(nb_asso_features * sparsity / 2)  # K=2
+        gamma_0 = np.zeros(nb_asso_features)
+        gamma_1 = gamma_0.copy()
+        gamma_0[0:nb_active_asso_features] = coeff_val
+        gamma_1[nb_active_asso_features + 1:
+                2 * nb_active_asso_features] = coeff_val
+
+        # Simulation of true times
+        T = np.empty(n_samples)
+        # n_samples_class_1 = np.sum(G)
+        # n_samples_class_0 = n_samples - n_samples_class_1
+
+        # T[G == 0] =
+        # T[G == 1] =
+
+        m = T.mean()
+        # Simulation of the censoring
+        c = self.censoring_factor
+        C = np.random.exponential(scale=c * m, size=n_samples)
+        # Observed time
+        self.times = np.minimum(T, C).astype(int)
+        # Censoring indicator: 1 if it is a time of failure, 0 if censoring
+        delta = (T <= C).astype(np.ushort)
+        self.censoring = delta
+
+        # Simulation of the measurement times using multivariate Hawkes
         a, b = adjacency_hawkes_uniform_bounds
         rvs = uniform(a, b).rvs
         adjacency = random(n_long_features, n_long_features, density=0.3,
@@ -359,18 +404,8 @@ class SimuJointLongitudinalSurvival(Simulation):
         hawkes.simulate()
         self.hawkes = hawkes
 
-        ##
+        # Simulation of the longitudinal features
+        Y = None
+        self.long_features = Y
 
-        # Simulation of true times
-        T = np.empty(n_samples)
-
-        m = T.mean()
-        # Simulation of the censoring
-        c = self.censoring_factor
-        C = np.random.exponential(scale=c * m, size=n_samples)
-        # Observed time
-        self.times = np.minimum(T, C).astype(int)
-        # Censoring indicator: 1 if it is a time of failure, 0 if censoring
-        censoring = (T <= C).astype(np.ushort)
-        self.censoring = censoring
-        return self.features, self.times, self.censoring
+        return X, Y, T, delta
