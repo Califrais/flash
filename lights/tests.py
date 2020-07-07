31593e98772a2500ac048fbb3f91c852134869d3
SimonBussy <simon.bussy@gmail.com>
2020-07-07 23:32:54 +0200

    update test_SimuJointLongitudinalSurvival

--- lights/tests.py
+++ lights/tests.py
@@ -12,31 +12,18 @@ class Test(unittest.TestCase):
         """Test simulation of joint longitudinal and survival data
         """
         # Simulate data with specific seed
-        simu = SimuJointLongitudinalSurvival(n_samples=1000,
-                                             n_time_indep_features=10,
-                                             n_long_features=10,
+        simu = SimuJointLongitudinalSurvival(n_samples=3,
+                                             n_time_indep_features=5,
+                                             n_long_features=3,
                                              seed=123, verbose=False)
         X_, Y_, T_, delta_ = simu.simulate()
 
-        T = np.array([
-            1.5022119, 5.93102441, 6.82837051, 0.50940341, 0.14859682,
-            30.22922996, 3.54945974, 0.8671229, 1.4228358, 0.11483298
-        ])
-
-        delta = np.array([1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-                             dtype=np.ushort)
-
-        X = np.array([[1.4912667, 0.80881799, 0.26977298], [
-            1.23227551, 0.50697013, 1.9409132
-        ], [1.8891494, 1.49834791,
-            2.41445794], [0.19431319, 0.80245126, 1.02577552], [
-                                 -1.61687582, -1.08411865, -0.83438387
-                             ], [2.30419894, -0.68987056,
-                                 -0.39750262],
-                             [-0.28826405, -1.23635074, -0.76124386], [
-                                 -1.32869473, -1.8752391, -0.182537
-                             ], [0.79464218, 0.65055633, 1.57572506],
-                             [0.71524202, 1.66759831, 0.88679047]])
+        T = np.array([23.4050397, 20.21906185, 15.93688448])
+        delta = np.array([0, 0, 1], dtype=np.ushort)
+        X = np.array(
+            [[-1.3854538, -1.4562842, -1.7882523, -1.387485, -1.3576753],
+             [-0.9514631, -2.1464529, -0.9993042, -1.420507,  0.8139507],
+             [0.7412221, -0.0128858,  0.2805381,  0.5195572,  0.6673799]])
 
         np.testing.assert_almost_equal(X, X_)
         np.testing.assert_almost_equal(T, T_)
