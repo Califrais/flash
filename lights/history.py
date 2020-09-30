a2c046930b9a137a6603a67ce7ac4a4953531925
SimonBussy <simon.bussy@gmail.com>
2020-09-30 09:22:38 +0200

    update history to separate printing from storage

--- lights/history.py
+++ lights/history.py
@@ -1,50 +1,37 @@
 # -*- coding: utf-8 -*-
 # Author: Simon Bussy <simon.bussy@gmail.com>
 
-import numpy as np
 from collections import defaultdict
 
 
-def n_iter_func(n_iter=0, **kwargs):
-    return n_iter
-
-
-def obj_func(obj=0, **kwargs):
-    return obj
-
-
-def rel_obj_func(rel_obj=0, **kwargs):
-    return rel_obj
-
-
-def spars_func(coeffs=None, **kwargs):
-    eps = np.finfo(coeffs.dtype).eps
-    return np.sum(np.abs(coeffs) > eps, axis=None)
-
-
 class History:
-    """A class to manage the history along iterations of a solver.
-    """
+    """A class to manage the history along iterations of a solver
 
-    def __init__(self, minimum_col_width=8,
-                 print_order=["n_iter", "obj", "rel_obj"]):
+    Parameters
+    ----------
+    minimum_col_width : `int`, default=8
+        Minimum of the column width for printing history
+
+    print_order : `list`, default=None
+        Gives in order the elements to be printed in print_history.
+        If None, then print_order = ["n_iter", "obj", "rel_obj"] which is the
+        common information we want to print for a solver : current iteration
+        number, current objective function value, and current relative objective
+        to monitor convergence
+    """
+    def __init__(self, minimum_col_width=8, print_order=None):
+        if print_order is None:
+            print_order = ["n_iter", "obj", "rel_obj"]
         self.minimum_col_width = minimum_col_width
         self.print_order = print_order
 
         # Instantiate values of the history
         self.clear()
 
-        # History function to compute history values based on parameters
-        # used in a solver
-        history_func = {}
-        history_func["n_iter"] = n_iter_func
-        history_func["obj"] = obj_func
-        history_func["rel_obj"] = rel_obj_func
-        self.history_func = history_func
-
         # Default print style of history values. Default is %.2e
-        print_style = defaultdict(lambda: "%g")
+        print_style = defaultdict(lambda: "%.2e")
         print_style["n_iter"] = "%d"
+        print_style["obj"] = "%g"
         self.print_style = print_style
 
         # Attributes that will be instantiated afterwards
@@ -58,40 +45,21 @@ class History:
 
     def update(self, **kwargs):
         """Update the history along the iterations.
-
-        For each keyword argument, we apply the history function corresponding
-        to this keyword, and use its results in the history
         """
         n_iter = kwargs["n_iter"]
         self.n_iter = n_iter
-        history_func = self.history_func
         history = self.values
-        for key, func in history_func.items():
-            history[key].append(func(**kwargs))
-
-    def set_print_order(self, *args):
-        """Allows to set the print order of the solver's history
-        """
-        self.print_order = list(*args)
-        self.clear()
-        return self
-
-    def update_history_func(self, **kwargs):
-        self.history_func.update(**kwargs)
-        self.clear()
-        return self
-
-    def update_print_style(self, **kwargs):
-        self.print_style.update(**kwargs)
-        return self
+        for key, val in kwargs.items():
+            history[key].append(val)
 
     def print_history(self):
-        """Verbose the current line of history
+        """Verbose for the current line of history regarding print_order
         """
         values = self.values
         n_iter = self.n_iter
         print_order = self.print_order
         # If this is the first iteration, plot the history's column names
+        # regarding print_order
         if n_iter == 0:
             min_width = self.minimum_col_width
             line = ' | '.join([name.center(min_width) for name in
@@ -107,12 +75,3 @@ class History:
                            for i, name in enumerate(print_order)
                            if name in values])
         print(line)
-
-    def get_values(self):
-        return self.values
-
-    def _add_end_history(self):
-        end_history = {}
-        self.end_history = end_history
-        for key, hist in self.history.items():
-            end_history[key] = hist[-1]
