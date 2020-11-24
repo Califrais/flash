92201843f0a8cf9ca835e4879f55015a2a5bc1d5
AcsiNguyen <tuan.nguyen2017@ict.jvn.edu.vn>
2020-11-24 23:56:08 +0100

    Add script to create testing data

--- lights/tests/test_asso.py
+++ lights/tests/test_asso.py
@@ -3,7 +3,8 @@
 
 import unittest
 import numpy as np
-from lights.tests.test_simu import get_testing_data
+
+from lights.tests.testing_data import CreateTestingData
 from lights.model.associations import AssociationFunctions
 from lights.model.e_step_functions import EstepFunctions
 from lights.base.base import extract_features
@@ -14,13 +15,13 @@ class Test(unittest.TestCase):
     def test_lp_asso(self):
         """Tests the the linear predictor association function
         """
-        X, Y, T, delta, simu = get_testing_data()
+        data = CreateTestingData()
+        X, Y, T, delta = data.X, data.Y, data.T, data.delta
         T_u = np.unique(T)
-        N = 2
-        alpha = simu.fixed_effect_time_order
-        L, p = simu.n_long_features, simu.n_time_indep_features
-        beta = simu.fixed_effect_coeffs
-        theta = simu.theta
+        N = data.N
+        alpha = data.fixed_effect_time_order
+        L, p = data.n_long_features, data.n_time_indep_features
+        theta = data.theta
 
         ext_feat = extract_features(Y, alpha)  # Features extraction
         asso_functions = 'all'
@@ -28,7 +29,10 @@ class Test(unittest.TestCase):
         E_func = EstepFunctions(X, T, delta, ext_feat, L, p, alpha,
                                 asso_functions, theta)
         S = E_func.construct_MC_samples(N)
+        beta = np.array([theta["beta_0"], theta["beta_1"]])
         asso_func = AssociationFunctions(T_u, S, beta, alpha, L)
+
+        #TODO: compute the expected value manually
 
 
 if __name__ == "main":
---
+++ lights/tests/testing_data.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Author: Simon Bussy <simon.bussy@gmail.com>
+
+
+import numpy as np
+import pandas as pd
+
+class CreateTestingData:
+    """A class to create testing data
+        """
+
+    def __init__(self):
+        self.n_long_features = 3
+        self.n_samples = 3
+        self.n_time_indep_features = 2
+        self.fixed_effect_time_order = 1
+        self.N = 2 # number of Monte-Carlo samples
+
+        self.X = np.array([[-1, 2], [2, 1], [0, 3]])
+        data = [[pd.Series(np.array([2, 3, 5]),index=[1, 2, 3]),
+                pd.Series(np.array([3, 4]),index=[1, 3]),
+                pd.Series(np.array([1, 3]),index=[2, 4])],
+                [pd.Series(np.array([2, 3]), index=[2, 4]),
+                 pd.Series(np.array([3, 4, 5]), index=[1, 3, 5]),
+                 pd.Series(np.array([2, 3, 6]), index=[1, 3, 4])],
+                [pd.Series(np.array([3, 5]), index=[2, 3]),
+                 pd.Series(np.array([1, 3, 4]), index=[1, 2, 5]),
+                 pd.Series(np.array([2, 3]), index=[2, 4])]]
+        columns = ['long_feature_%s' % (l + 1) for l in range(self.n_long_features)]
+        self.Y = pd.DataFrame(data=data, columns=columns)
+        self.T = np.array([22, 19, 12])
+        self.delta = np.array([1, 0, 1], dtype=np.ushort)
+        baseline_hazard = pd.Series(data=np.array([5, 8, 12]), index=self.T)
+        beta_0 = np.array([[1, 2], [-3, 2], [-1, 3]])
+        beta_1 = np.array([[-1, -2], [2, 3], [1, 2]])
+        gamma_0 = np.array([-1, 1, 1, 0, 2, -1, 1, 2, -2, 3, -1, 0, -2, 1, 3, 2, 0])
+        gamma_1 = np.array([2, -2, 1, 1, -3, 1, 2, -1, -1, -3, 1, 1, 0, 2, 1, 3, -1])
+        D = np.array([[2, 1, 3], [1, 3, 4], [3, 4, 1]])
+        phi = np.array([1, 2, 3])
+        xi = np.array([1, 2])
+        self.theta = {
+            "beta_0": beta_0,
+            "beta_1": beta_1,
+            "long_cov": D,
+            "phi": phi,
+            "xi": xi,
+            "baseline_hazard": baseline_hazard,
+            "gamma_0": gamma_0,
+            "gamma_1": gamma_1
+        }
