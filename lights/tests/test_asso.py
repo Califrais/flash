6898a3548c89156b28016fa8cda5f1eb2a6117f6
AcsiNguyen <tuan.nguyen2017@ict.jvn.edu.vn>
2020-11-25 16:21:50 +0100

    Update the association functions testing

--- lights/tests/test_asso.py
+++ lights/tests/test_asso.py
@@ -6,15 +6,11 @@ import numpy as np
 
 from lights.tests.testing_data import CreateTestingData
 from lights.model.associations import AssociationFunctions
-from lights.model.e_step_functions import EstepFunctions
-from lights.base.base import extract_features
 
-
-class Test(unittest.TestCase):
-
-    def test_lp_asso(self):
-        """Tests the the linear predictor association function
-        """
+class Asso_Test():
+    """A class to create testing association functions
+    """
+    def __init__(self):
         data = CreateTestingData()
         X, Y, T, delta = data.X, data.Y, data.T, data.delta
         T_u = np.unique(T)
@@ -22,18 +18,76 @@ class Test(unittest.TestCase):
         alpha = data.fixed_effect_time_order
         L, p = data.n_long_features, data.n_time_indep_features
         theta = data.theta
-
-        ext_feat = extract_features(Y, alpha)  # Features extraction
-        asso_functions = 'all'
-
-        E_func = EstepFunctions(X, T, delta, ext_feat, L, p, alpha,
-                                asso_functions, theta)
-        S = E_func.construct_MC_samples(N)
         beta = np.array([theta["beta_0"], theta["beta_1"]])
-        asso_func = AssociationFunctions(T_u, S, beta, alpha, L)
+        self.asso_func = AssociationFunctions(T_u, data.S, beta, alpha, L)
 
-        #TODO: compute the expected value manually
 
+class Test(unittest.TestCase):
+    """
+    A class to test association functions
+    """
+    def test_lp_asso(self):
+        """Tests the the linear predictor association function
+        """
+        asso_func = Asso_Test().asso_func
+        phi = asso_func.linear_predictor()
+        phi_0_1 = np.array([[482, 492, 495, 478],
+                         [1161, 1178, 1181, 1157],
+                         [1542, 1562, 1565, 1538]])
+        phi_1_3 = np.array([[-80, -102, -154, -103],
+                            [-262, -298, -378, -299],
+                            [-370, -412, -504, -413]])
+        np.testing.assert_almost_equal(phi[0, :, :, 0], phi_0_1)
+        np.testing.assert_almost_equal(phi[1, :, :, 2], phi_1_3)
+
+    def test_re_asso(self):
+        """Tests the the random effects association function
+        """
+        asso_func = Asso_Test().asso_func
+        phi = asso_func.random_effects()
+        #TODO: Compute expected value manually
+
+    def test_tps_asso(self):
+        """Tests the the time dependent slope association function
+        """
+        asso_func = Asso_Test().asso_func
+        phi = asso_func.time_dependent_slope()
+        #TODO: Compute expected value manually
+
+    def test_ce_asso(self):
+        """Tests the the cumulative effects association function
+        """
+        asso_func = Asso_Test().asso_func
+        phi = asso_func.cumulative_effects()
+        # TODO: Compute expected value manually
+
+    def test_dlp_asso(self):
+        """Tests the the derivative linear predictor association function
+        """
+        asso_func = Asso_Test().asso_func
+        phi = asso_func.derivative_linear_predictor()
+        # TODO: Compute expected value manually
+
+    def test_dre_asso(self):
+        """Tests the the derivative random effects association function
+        """
+        asso_func = Asso_Test().asso_func
+        phi = asso_func.derivative_random_effects()
+        # TODO: Compute expected value manually
+
+    def test_dtps_asso(self):
+        """Tests the the derivative time dependent slope association function
+        """
+        asso_func = Asso_Test().asso_func
+        phi = asso_func.derivative_time_dependent_slope()
+        # TODO: Compute expected value manually
+
+    def test_dce_asso(self):
+        """Tests the the derivative cumulative effects association function
+        """
+        asso_func = Asso_Test().asso_func
+        phi = asso_func.derivative_cumulative_effects()
+        # TODO: Compute expected value manually
 
 if __name__ == "main":
     unittest.main()
--- lights/tests/testing_data.py
+++ lights/tests/testing_data.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import pandas as pd
+from lights.base.base import extract_features
 
 class CreateTestingData:
     """A class to create testing data
@@ -13,7 +14,7 @@ class CreateTestingData:
         self.n_long_features = 3
         self.n_samples = 3
         self.n_time_indep_features = 2
-        self.fixed_effect_time_order = 1
+        self.fixed_effect_time_order = 2
         self.N = 2 # number of Monte-Carlo samples
 
         self.X = np.array([[-1, 2], [2, 1], [0, 3]])
@@ -31,11 +32,18 @@ class CreateTestingData:
         self.T = np.array([22, 19, 12])
         self.delta = np.array([1, 0, 1], dtype=np.ushort)
         baseline_hazard = pd.Series(data=np.array([5, 8, 12]), index=self.T)
-        beta_0 = np.array([[1, 2], [-3, 2], [-1, 3]])
-        beta_1 = np.array([[-1, -2], [2, 3], [1, 2]])
-        gamma_0 = np.array([-1, 1, 1, 0, 2, -1, 1, 2, -2, 3, -1, 0, -2, 1, 3, 2, 0])
-        gamma_1 = np.array([2, -2, 1, 1, -3, 1, 2, -1, -1, -3, 1, 1, 0, 2, 1, 3, -1])
-        D = np.array([[2, 1, 3], [1, 3, 4], [3, 4, 1]])
+        beta_0 = np.array([1, 2, 3, -3, 2, 2, -1, 3, -1]).reshape(-1, 1)
+        beta_1 = np.array([-1, -2, 2, 2, 3, 1, 1, 2, -1]).reshape(-1, 1)
+        gamma_0 = np.array([-1, 1, 1, 0, 2, -1, 1, 2, -2, 3, -1, 0,
+                            -2, 1, 3, 2, 0]).reshape(-1, 1)
+        gamma_1 = np.array([2, -2, 1, 1, -3, 1, 2, -1, -1, -3, 1, 1,
+                            0, 2, 1, 3, -1]).reshape(-1, 1)
+        D = np.array([[2, 1, 3, 3, 2, 3],
+                      [1, 3, 4, 4, 3, 1],
+                      [3, 4, 1, 2, 1, 2],
+                      [3, 4, 2, 2, 2, 2],
+                      [2, 3, 1, 2, 2, 1],
+                      [3, 1, 2, 2, 1, 1]])
         phi = np.array([1, 2, 3])
         xi = np.array([1, 2])
         self.theta = {
@@ -48,3 +56,9 @@ class CreateTestingData:
             "gamma_0": gamma_0,
             "gamma_1": gamma_1
         }
+        # Features extraction
+        self.ext_feat = extract_features(self.Y, self.fixed_effect_time_order)
+        self.S = np.array([[1, 2, 3, 2, 3, 3],
+                      [-1, 3, 2, 4, 5, 1],
+                      [2, 3, -1, -2, 1, -3],
+                      [-3, 2, 3, -3, 4, 1]])
