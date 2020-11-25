50912d5f4858fec9031f0c0445615c0c0274a6c1
AcsiNguyen <tuan.nguyen2017@ict.jvn.edu.vn>
2020-11-25 22:08:40 +0100

    Update the association functions testing #2

--- lights/tests/test_asso.py
+++ lights/tests/test_asso.py
@@ -45,49 +45,73 @@ class Test(unittest.TestCase):
         """
         asso_func = Asso_Test().asso_func
         phi = asso_func.random_effects()
-        #TODO: Compute expected value manually
+        phi_0_0 = np.array([[1, 2, 3, 2, 3, 3],
+                      [-1, 3, 2, 4, 5, 1],
+                      [2, 3, -1, -2, 1, -3],
+                      [-3, 2, 3, -3, 4, 1]])
+        np.testing.assert_almost_equal(phi[0, 0], phi_0_0)
 
     def test_tps_asso(self):
         """Tests the the time dependent slope association function
         """
         asso_func = Asso_Test().asso_func
         phi = asso_func.time_dependent_slope()
-        #TODO: Compute expected value manually
+        phi_0_1 = np.array([[76, 77, 77, 76],
+                            [118, 119, 119, 118],
+                            [136, 137, 137, 136]])
+        phi_1_3 = np.array([[-19, -21, -25, -21],
+                            [-33, -35, -39, -35],
+                            [-39, -41, -45, -41]])
+        np.testing.assert_almost_equal(phi[0, :, :, 0], phi_0_1)
+        np.testing.assert_almost_equal(phi[1, :, :, 2], phi_1_3)
 
     def test_ce_asso(self):
         """Tests the the cumulative effects association function
         """
         asso_func = Asso_Test().asso_func
         phi = asso_func.cumulative_effects()
-        # TODO: Compute expected value manually
+        phi_0_1 = np.array([[2040, 2088, 2124, 1992],
+                            [7619, 7761.5, 7818.5, 7543],
+                            [11660, 11858, 11924, 11572]])
+        np.testing.assert_almost_equal(phi[0, :, :, 0], phi_0_1)
 
     def test_dlp_asso(self):
         """Tests the the derivative linear predictor association function
         """
         asso_func = Asso_Test().asso_func
         phi = asso_func.derivative_linear_predictor()
-        # TODO: Compute expected value manually
+        phi_0_1 = np.array([[1, 12, 144],
+                           [1, 19, 361],
+                           [1, 22, 484]])
+        np.testing.assert_almost_equal(phi[0, 0, :, 0], phi_0_1)
 
     def test_dre_asso(self):
         """Tests the the derivative random effects association function
         """
         asso_func = Asso_Test().asso_func
         phi = asso_func.derivative_random_effects()
-        # TODO: Compute expected value manually
+        phi_0_1 = np.zeros((3, 6))
+        np.testing.assert_almost_equal(phi[0, 0, :, 0], phi_0_1)
 
     def test_dtps_asso(self):
         """Tests the the derivative time dependent slope association function
         """
         asso_func = Asso_Test().asso_func
         phi = asso_func.derivative_time_dependent_slope()
-        # TODO: Compute expected value manually
+        phi_0_1 = np.array([[0, 1, 24],
+                            [0, 1, 38],
+                            [0, 1, 44]])
+        np.testing.assert_almost_equal(phi[0, 0, :, 0], phi_0_1)
 
     def test_dce_asso(self):
         """Tests the the derivative cumulative effects association function
         """
         asso_func = Asso_Test().asso_func
         phi = asso_func.derivative_cumulative_effects()
-        # TODO: Compute expected value manually
+        phi_0_1 = np.array([[12, 72, 576],
+                            [19, 180.5, 6859/3],
+                            [22, 242, 10648/3]])
+        np.testing.assert_almost_equal(phi[0, 0, :, 0], phi_0_1)
 
 if __name__ == "main":
     unittest.main()
