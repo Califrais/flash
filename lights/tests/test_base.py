7c9ceb39b6ea9c831562ba52b45435a2652b0395
SimonBussy <simon.bussy@gmail.com>
2020-11-27 14:17:21 +0100

    add test base file

---
+++ lights/tests/test_base.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Author: Simon Bussy <simon.bussy@gmail.com>
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_extract_features(self):
+        """TODO
+        """
+
+
+if __name__ == "main":
+    unittest.main()
