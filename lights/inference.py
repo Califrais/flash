eb1e23fb721f94dc3e21cb985f3ab7142a3c200c
AcsiNguyen <tuan.nguyen2017@ict.jvn.edu.vn>
2020-12-03 15:27:48 +0100

    Refactor m_step_functions

--- lights/inference.py
+++ lights/inference.py
@@ -414,19 +414,19 @@ class QNMCEM(Learner):
 
             # xi update
             xi_ext = fmin_l_bfgs_b(
-                func=lambda xi_ext_: F_func.P_func(pi_est, xi_ext_), x0=xi_init,
-                fprime=lambda xi_ext_: F_func.grad_P(pi_est, xi_ext_),
+                func=lambda xi_ext_: F_func.P_pen_func(pi_est, xi_ext_), x0=xi_init,
+                fprime=lambda xi_ext_: F_func.grad_P_pen(pi_est, xi_ext_),
                 disp=False, bounds=bounds_xi, maxiter=maxiter, pgtol=pgtol)[0]
 
             # beta update
             pi_est_K = [1 - pi_est, pi_est]
             [beta_0_ext, beta_1_ext] = [fmin_l_bfgs_b(
                 func=lambda beta_ext:
-                F_func.R_func(beta_ext, pi_est_K[k], E_g1.T[k].T, E_g2.T[k].T,
+                F_func.R_pen_func(beta_ext, pi_est_K[k], E_g1.T[k].T, E_g2.T[k].T,
                               E_g9.T[k].T, baseline_hazard, ind_2),
                 x0=beta_init[k],
                 fprime=lambda beta_ext:
-                F_func.grad_R(beta_ext, gamma_0_ext, pi_est_K[k], E_g5.T[k].T,
+                F_func.grad_R_pen(beta_ext, gamma_0_ext, pi_est_K[k], E_g5.T[k].T,
                               E_g6.T[k].T, E_gS, baseline_hazard, ind_2,
                               ext_feat, phi),
                 disp=False, bounds=bounds_beta, maxiter=maxiter,
@@ -445,11 +445,11 @@ class QNMCEM(Learner):
             # gamma update
             [gamma_0_ext, gamma_1_ext] = [fmin_l_bfgs_b(
                 func=lambda gamma_ext:
-                F_func.Q_func(gamma_ext, pi_est[k], E_log_g1.T[k].T, E_g1.T[k].T,
+                F_func.Q_pen_func(gamma_ext, pi_est[k], E_log_g1.T[k].T, E_g1.T[k].T,
                               baseline_hazard, ind_1, ind_2),
                 x0=gamma_init[k],
                 fprime=lambda gamma_ext:
-                F_func.grad_Q(gamma_ext, pi_est[k], E_g1.T[k].T, E_g7.T[k].T,
+                F_func.grad_Q_pen(gamma_ext, pi_est[k], E_g1.T[k].T, E_g7.T[k].T,
                               E_g8.T[k].T, baseline_hazard, ind_1, ind_2),
                 disp=False, bounds=bounds_gamma, maxiter=maxiter,
                 pgtol=pgtol)[0].reshape(-1, 1)
