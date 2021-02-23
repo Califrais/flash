75783c0c1c1f792331600f9df9584cacee2dc017
AcsiNguyen <tuan.nguyen2017@ict.jvn.edu.vn>
2021-02-23 21:46:17 +0100

    Convert type of number Monte Carlo samples from float to integer

--- lights/inference.py
+++ lights/inference.py
@@ -581,7 +581,7 @@ class QNMCEM(Learner):
             con = (np.mean(rel_theta_list[1:]) / np.std(rel_theta_list[1:])) > \
                   (np.mean(rel_theta_list[:3]) / np.std(rel_theta_list[:3]))
             if con:
-                N *= 1.1
+                N = int(N * 1.2)
 
             # M-Step
             D = E_g4.sum(axis=0) / n_samples  # D update
