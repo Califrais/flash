c87120eb8505fba0910dd1fc35e63009eef91595
simon <sim.maby@gmail.com>
2021-07-15 11:01:18 +0200

    Adding setup.py and a configurable notebook for experiments

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='lights',
+    version='0.1',
+    author="Van-Tuan Nguyen",
+    description="ligths is a generalized joint model for high-dimensional multivariate longitudinal data and censored durations",
+    url="https://github.com/Califrais/lights",
+    )
