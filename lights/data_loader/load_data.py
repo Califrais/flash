c0e0360a3f3de128a80171fa4f1bce67d10a6a4d
NVTACSI <tuan.nguyen2017@ict.jvn.edu.vn>
2022-09-07 11:48:49 +0700

    Inactive the R enviroment (to be updated later)

--- lights/data_loader/load_data.py
+++ lights/data_loader/load_data.py
@@ -1,5 +1,5 @@
 import os
-os.environ['R_HOME'] = "/Library/Frameworks/R.framework/Resources"
+#os.environ['R_HOME'] = "/Library/Frameworks/R.framework/Resources"
 from rpy2 import robjects
 from rpy2.robjects import pandas2ri, numpy2ri
 import numpy as np
