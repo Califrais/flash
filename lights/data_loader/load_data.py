780c71cda6b5f3d56b1c672e56f3b38ab475cd58
NVTACSI <tuan.nguyen2017@ict.jvn.edu.vn>
2022-10-07 15:20:53 +0200

    Refactor data loader

--- lights/data_loader/load_data.py
+++ lights/data_loader/load_data.py
@@ -10,40 +10,22 @@ def load_data(simu, seed):
     if simu:
         n_long_features = 5
         n_time_indep_features = 10
-        n_samples = 200
+        n_samples = 300
         simu = SimuJointLongitudinalSurvival(seed=seed,
                                              n_long_features=n_long_features,
                                              n_samples=n_samples,
                                              n_time_indep_features=n_time_indep_features,
                                              sparsity=0.5, grid_time=True)
-        X, Y, T, delta, _, Y_rep, t_max = simu.simulate()
-        id = np.arange(n_samples)
+        X, Y, T, delta = simu.simulate()
+        id = np.unique(Y.id.values)
         time_dep_feat = ['long_feature_%s' % (l + 1)
                          for l in range(n_long_features)]
         time_indep_feat = ['X_%s' % (l + 1)
                            for l in range(n_time_indep_features)]
 
-        data_lights = pd.DataFrame(data=np.column_stack((id, T, delta, X, Y)),
-                            columns=["id", "T_survival", "delta"] +
-                                    time_indep_feat + time_dep_feat)
         df1 = pd.DataFrame(data=np.column_stack((id, T, delta, X)),
                            columns=["id", "T_survival", "delta"] + time_indep_feat)
-        for i in range(n_samples):
-            Y_i_ = []
-            for l in range(n_long_features):
-                Y_il = Y.loc[i][l]
-                times_il = Y_il.index.values
-                y_il = Y_il.values.flatten().tolist()
-                n_il = len(times_il)
-                Y_i_.append(y_il)
-            Y_i = np.column_stack(
-                (np.array([id[i]] * n_il), times_il, np.array([t_max[i]] * n_il), np.array(Y_i_).T))
-            if i == 0:
-                Y_ = Y_i
-            else:
-                Y_ = np.row_stack((Y_, Y_i))
-        data_lights["T_max"] = t_max
-        df2 = pd.DataFrame(data=Y_, columns=["id", "T_long", "T_max"] + time_dep_feat)
+        df2 = pd.DataFrame(data=Y.values, columns=["id", "T_long"] + time_dep_feat)
         data = pd.merge(df2, df1, on="id")
     else:
         # load PBC Seq
@@ -102,13 +84,14 @@ def load_data(simu, seed):
         data_lights[time_dep_feat] = Y
         data_lights["T_max"] = t_max
         data["T_max"] = np.array(t_max_R).flatten()
-    return (data, data_lights, Y_rep, time_dep_feat, time_indep_feat)
+    return data, time_indep_feat, time_dep_feat
 
 def extract_lights_feat(data, time_indep_feat, time_dep_feat):
-    X = np.float_(data[time_indep_feat].values)
-    Y = data[time_dep_feat]
-    T = np.float_(data[["T_survival"]].values.flatten())
-    delta = np.int_(data[["delta"]].values.flatten())
+    Y = data[["id", "T_long"] + time_dep_feat]
+    data_id = data.drop_duplicates(subset=["id"])
+    X = np.float_(data_id[time_indep_feat].values)
+    T = np.float_(data_id[["T_survival"]].values.flatten())
+    delta = np.int_(data_id[["delta"]].values.flatten())
 
     return (X, Y, T, delta)
 
