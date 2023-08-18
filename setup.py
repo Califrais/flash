ed18849c393889f2fd38cc7fab4e4c7a6b2cda74
NVTACSI <tuan.nguyen2017@ict.jvn.edu.vn>
2023-08-18 12:53:56 +0200

    Update requirement

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 
 setup(
-    name='lights',
+    name='flash',
     version='0.1',
     author="Van-Tuan Nguyen",
-    description="ligths is a generalized joint model for high-dimensional multivariate longitudinal data and censored durations",
-    url="https://github.com/Califrais/lights",
+    description="flash is a generalized joint model for high-dimensional multivariate longitudinal data and censored durations",
+    url="https://github.com/Califrais/flash",
     )
